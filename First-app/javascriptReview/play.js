c7a9ac954a0991089658a76d6aa53b6e6d35e499
Th3lios <earayad@hotmail.cl>
2019-08-23 10:27:58 -0400

    Async

--- First-app/javascriptReview/play.js
+++ First-app/javascriptReview/play.js
@@ -1,6 +1,3 @@
-var name = "Max";
-var age = 23;
-var hasHobbies = true;
 
 /* Object */
 const person = {
@@ -17,6 +14,55 @@ const person = {
     }
 };
 
+/* Arrays */
 
+const hobbies = ["sport","paint"];
+
+for (let hobbie of hobbies){
+    console.log(hobbie);
+}
+
+const array = (...args) =>{
+    return args
+}
+console.log(array(1,2,3,4));
+
+/* Spread */
+
+const hbb = [...hobbies];
+console.log(hbb);
+
+/* Destructure */
+
+const printName = ({name}) =>{
+    console.log(name);
+}
+
+printName(person);
+
+const {name,age} = person;
+console.log(name,age);
+
+const [hobby1,hobby2] = hobbies;
+console.log(hobby1,hobby2);
+
+/* async */
+
+const fetchData = callback => {
+    setTimeout(()=>{
+        callback("done");
+    },2000)
+}
+
+setTimeout( callback => {
+    console.log("ready")
+    fetchData(text => {
+        console.log(text)
+    })
+},2000)
+
+/*setInterval(() =>{
+    console.log("hello");
+},2000);*/
 
 console.log(person.sumarizeUser(person.name,person.age,person.hobbie));
